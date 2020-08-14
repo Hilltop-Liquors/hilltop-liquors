41de27dccbeb9e8e134952047414e63746b60a5d
Julia Contreras <juliacontreras483@gmail.com>
2020-08-14 10:10:17 -0500

    added gitignore

---
+++ src/main/java/com/codeup/hilltopliquors/HilltopLiquorsApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.hilltopliquors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HilltopLiquorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HilltopLiquorsApplication.class, args);
+    }
+
+}
