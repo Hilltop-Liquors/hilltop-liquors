1b826721009feea5e234a85e84b66b16ca704431
Julia Contreras <juliacontreras483@gmail.com>
2020-08-17 15:35:54 -0500

    nav links/html

--- src/main/java/com/codeup/hilltopliquors/controllers/HelloWorldController.java
+++ src/main/java/com/codeup/hilltopliquors/controllers/HelloWorldController.java
@@ -1,14 +1,14 @@
-package com.codeup.hilltopliquors.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-public class HelloWorldController {
-    @GetMapping("/")
-    @ResponseBody
-    public String hello() {
-        return "Hilltop Liquors";
-    }
-}
+//package com.codeup.hilltopliquors.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class HelloWorldController {
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hilltop Liquors";
+//    }
+//}
---
+++ src/main/java/com/codeup/hilltopliquors/controllers/HomeController.java
@@ -0,0 +1,43 @@
+package com.codeup.hilltopliquors.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String showHome() {
+        return "home";
+    }
+    @GetMapping("/About")
+    public String showAbout() {
+        return "about";
+    }
+    @GetMapping("/Cart")
+    public String showCart() {
+        return "cart";
+    }
+    @GetMapping("/Catering")
+    public String showCatering() {
+        return "catering";
+    }
+    @GetMapping("/Contact")
+    public String showContact() {
+        return "contact";
+    }
+    @GetMapping("/Events")
+    public String showEvents() {
+        return "events";
+    }
+    @GetMapping("/Login")
+    public String showLogin() {
+        return "login";
+    }
+    @GetMapping("/Recipes")
+    public String showRecipes() {
+        return "recipes";
+    }
+    @GetMapping("/SignUp")
+    public String showSignUp() {
+        return "signup";
+    }
+}
