663e4b734944a577824c23c3ef6a94bf4e5aee9d
Shelby Polasek <shelbypolasek@gmail.com>
2020-08-17 11:15:04 -0500

    price changed to double in products table

--- database/migration.sql
+++ database/migration.sql
@@ -1,4 +1,5 @@
 
+CREATE DATABASE IF NOT EXISTS hilltop_db;
 
 # PRODUCTS TABLE
 
@@ -11,12 +12,10 @@ CREATE TABLE `products` (
                             PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-#  PRODUCT TYPE TABLES (broadest cats)
+#  PRODUCT_TYPES TABLES (broadest cats)
 CREATE TABLE `product_types` (
                             `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
                             `name` varchar(255),
-                            `size` varchar(100),
-                            `price_in_cents` decimal,
-                            `in_store_count` int,
                             PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
