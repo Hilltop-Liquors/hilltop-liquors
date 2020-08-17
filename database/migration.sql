f3c918f00783fe1b82bda14864da244683bdc47d
Shelby Polasek <shelbypolasek@gmail.com>
2020-08-17 17:18:45 -0500

    products table

--- database/migration.sql
+++ database/migration.sql
@@ -3,22 +3,29 @@ SHOW DATABASES;
 CREATE DATABASE IF NOT EXISTS hilltop_test_db;
 USE hilltop_test_db;
 
+# DROP DATABASE hilltop_test_db;
+
 SHOW TABLES;
 SELECT * FROM products;
 SELECT * FROM product_types;
 SELECT * FROM categories;
 SELECT * FROM sub_categories;
-# DROP TABLE products;
+# SELECT * FROM cats_mapping;
+DROP TABLE products;
 DROP TABLE categories;
+DROP TABLE sub_categories;
+# DROP TABLE cats_mapping;
 
 # PRODUCTS TABLE
 CREATE TABLE `products` (
                             `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                            `sub_cat_id` int(11) unsigned,
                             `name` varchar(255),
                             `size` varchar(100),
                             `price_in_cents` double,
                             `in_store_count` int,
-                            PRIMARY KEY (`id`)
+                            PRIMARY KEY (`id`),
+                            FOREIGN KEY (sub_cat_id) REFERENCES sub_categories(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -29,31 +36,34 @@ CREATE TABLE `product_types` (
                             PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 # CATEGORIES
 CREATE TABLE `categories` (
                              `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
                              `name` varchar(255),
-                             PRIMARY KEY (`id`)
+                             `product_type_id` int(11) unsigned,
+                              PRIMARY KEY (`id`),
+                              FOREIGN KEY (product_type_id) REFERENCES product_types(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 # SUB CATEGORIES
 CREATE TABLE `sub_categories` (
                              `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
                              `name` varchar(255),
-                             PRIMARY KEY (`id`)
+                             `cat_id` int(11) unsigned,
+                             PRIMARY KEY (`id`),
+                             FOREIGN KEY(cat_id) REFERENCES categories(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 # MAPPING
-CREATE TABLE `cats_mapping` (
-                                 `products_id` int(11) unsigned NOT NULL,
-                                 `product_types_id` int(11) unsigned NOT NULL,
-                                 `cat_id` int(11) unsigned NOT NULL,
-                                 `sub_cat_id` int(11) unsigned,
-                                 FOREIGN KEY (products_id) REFERENCES products(id),
-                                 FOREIGN KEY (product_types_id) REFERENCES product_types(id),
-                                 FOREIGN KEY (cat_id) REFERENCES categories(id),
-                                 FOREIGN KEY (sub_cat_id) REFERENCES sub_categories(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+# CREATE TABLE `cats_mapping` (
+#                                  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+#                                  `product_types_id` int(11) unsigned NOT NULL,
+#                                  `cat_id` int(11) unsigned NOT NULL,
+#                                  `sub_cat_id` int(11) unsigned,
+#                                  PRIMARY KEY (`id`),
+#                                  FOREIGN KEY (product_types_id) REFERENCES product_types(id),
+#                                  FOREIGN KEY (cat_id) REFERENCES categories(id),
+#                                  FOREIGN KEY (sub_cat_id) REFERENCES sub_categories(id)
+# ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
