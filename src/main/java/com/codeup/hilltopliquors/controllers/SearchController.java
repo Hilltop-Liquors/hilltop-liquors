05a6604fee87d7e8c167e796b8828d3831918d44
Shelby Polasek <shelbypolasek@gmail.com>
2020-08-21 20:47:12 -0500

    working on cat search

--- src/main/java/com/codeup/hilltopliquors/controllers/SearchController.java
+++ src/main/java/com/codeup/hilltopliquors/controllers/SearchController.java
@@ -3,6 +3,7 @@ package com.codeup.hilltopliquors.controllers;
 import com.codeup.hilltopliquors.models.Category;
 import com.codeup.hilltopliquors.models.Product;
 import com.codeup.hilltopliquors.models.ProductType;
+import com.codeup.hilltopliquors.models.Subcategory;
 import com.codeup.hilltopliquors.repositories.CategoryRepository;
 import com.codeup.hilltopliquors.repositories.ProductRepository;
 import com.codeup.hilltopliquors.repositories.ProductTypeRepository;
@@ -31,36 +32,31 @@ public class SearchController {
 
 //        GET ALL POSTS and SEARCH BAR
         @GetMapping("/Search")
-        public String getProducts(Model model, String keyword){
+        public String getProducts(Model model, String keyword, long id){
                 List<Product> products = productDao.findAll();
-
 
                 if(keyword != null){
                         model.addAttribute("products", productDao.findByKeyWord(keyword));
                 }
-//                else if() {
-//
-//                }
                 else {
                         model.addAttribute("products", products);
                 }
                 return "search/search";
         }
 
-        @GetMapping("/Search/Beer/{id}")
+        @GetMapping("/Search/find/{id}")
         public String getBeer(@PathVariable(value = "id") int id, Model model) {
-                ProductType productType = productTypeDao.getOne(id);
-                List<Category>
+                ProductType getProductType = productTypeDao.getOne(id);
+                List<Category> getCategory = productTypeDao.getOne(id).getCategories();
 
+
+                model.addAttribute("categories", getCategory);
+                model.addAttribute("productType", getProductType);
+
+                return "/search/find";
 
         }
-                Post indPost = postsDao.getOne(id);
-                List<Comment> getComments = postsDao.getOne(id).getComments();
 
-                model.addAttribute("comments", getComments);
-                model.addAttribute("post", indPost);
-
-                return "posts/show";
 
 
 
