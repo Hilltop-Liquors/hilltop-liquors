7de6a6323afd98ad9e7a0b7c2009c104157e0134
Andrew Brought <andrewbrought@gmail.com>
2020-08-23 20:10:54 -0500

    figured out how to get pull the authenticated users credentials and plug them into the update form

--- src/main/java/com/codeup/hilltopliquors/controllers/UserEditController.java
+++ src/main/java/com/codeup/hilltopliquors/controllers/UserEditController.java
@@ -1,6 +1,11 @@
 package com.codeup.hilltopliquors.controllers;
 
+import com.codeup.hilltopliquors.models.User;
 import com.codeup.hilltopliquors.repositories.UserRepository;
+import com.codeup.hilltopliquors.services.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,15 +14,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class UserEditController {
 
-    private UserRepository userDao;
+    @Autowired
+    UserServiceImpl userService;
+
+    private final UserRepository userDao;
 
     public UserEditController(UserRepository userDao) {
         this.userDao = userDao;
     }
 
-    @GetMapping("/user/edit/{id}")
-    public String showEditForm(@PathVariable long id, Model model) {
-        model.addAttribute("user", userDao.getOne(id));
+    @GetMapping("/user/edit")
+    public String showEditForm(Model model, Authentication authentication) {
+//        User user = userDao.getOne(1);
+//        System.out.println(user);
+//        System.out.println("This is the SOUT:" + SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+//        System.out.println("hello from showEditForm");
+//
+//        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        String username = authentication.getName();
+        User currentUser = userDao.findByUsername(username);
+        System.out.println("THIS IS THE SOUT:" + currentUser);
+       model.addAttribute("user", currentUser);
+
         return "user/edit";
     }
 
