df38a4b1ad6b80a7bd6f1835d4d15017da5c1a2c
Andrew Brought <andrewbrought@gmail.com>
2020-08-26 11:34:28 -0500

    was able to separate users and admins, still need to figure out issue with password in edit user

--- src/main/java/com/codeup/hilltopliquors/controllers/AdminController.java
+++ src/main/java/com/codeup/hilltopliquors/controllers/AdminController.java
@@ -1,30 +1,35 @@
-//package com.codeup.hilltopliquors.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//
-//import javax.servlet.http.HttpServletRequest;
-//
-//@Controller
-//public class AdminController {
-//
-//
-//    @RequestMapping(method = RequestMethod.GET)
+package com.codeup.hilltopliquors.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class AdminController {
+
+
 //    public String showUserDashboard(Model viewModel, HttpServletRequest request){
-//
-//        if(request.isUserInRole("ROLE_ADMIN")) {
-//
-//            return"redirect/admin/home";
-//
-//
+//    //.... controller code here
+//        if(request.isUserInRole("ROLE_CONTRACTOR")){
+//            viewModel.addAttribute(//what goes in here??)
+//            return"redirect:/admin";
 //        } else {
 //            return "/Home";
 //        }
+//
 //    }
-//
-//
-//
-//}
+
+
+    @GetMapping("/admin")
+    public String showAdmin(HttpServletRequest request) {
+        if (request.isUserInRole("ROLE_ADMIN")) {
+            return "/admin/admin-page";
+        } else {
+            return "redirect:/Home";
+        }
+    }
+}
