d00b5966980295d474b6ec13b485a238fc35879d
Shelby Polasek <shelbypolasek@gmail.com>
2020-08-26 11:40:01 -0500

    checkout connected

--- src/main/java/com/codeup/hilltopliquors/controllers/CartController.java
+++ src/main/java/com/codeup/hilltopliquors/controllers/CartController.java
@@ -36,12 +36,23 @@ public class CartController {
     @ModelAttribute("order")
     public Order order() {
         Order newOrder = new Order();
-//        User user =(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Instant instant = Instant.now();
         Timestamp timestamp = Timestamp.from(instant);
         newOrder.setCreatedAt(timestamp);
-//        newOrder.setUser(user);
+
         return newOrder;
+    }
+
+    @ModelAttribute("orderDetails")
+    public List<String> order(HttpServletRequest request) {
+        List<String> orderDetails;
+        if (request.getSession().getAttribute("orderDetails") == null) {
+            orderDetails = new ArrayList<>();
+        } else {
+            orderDetails = (List<String>) request.getSession().getAttribute("orderDetails");
+        }
+        request.getSession().setAttribute("orderDetails", orderDetails);
+        return orderDetails;
     }
 
 
@@ -78,17 +89,12 @@ public class CartController {
     }
 
     @PostMapping("/Cart/confirm-details")
-    public String saveCheckoutDetails(Model model, @SessionAttribute("cart") List<Product> cart, String pickUpDate, boolean isCurbside, String pickupTime) {
-        Order newOrder = new Order();
-//        User user =(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Instant instant = Instant.now();
-        Timestamp timestamp = Timestamp.from(instant);
-        newOrder.setCreatedAt(timestamp);
-        System.out.println("WHAT TIME IS IT!!!!!!!!!! " + timestamp);
-        System.out.println("HERE WE ARE" + pickUpDate);
-        System.out.println("HERE WE ARE" + isCurbside);
-        System.out.println("HERE WE ARE" + pickupTime);
+    public String saveCheckoutDetails( Model model ,@SessionAttribute("cart") List<Product> cart ,@SessionAttribute("orderDetails") List<String> orderDetails, String pickUpDate, String isCurbside, String pickupTime) {
+        orderDetails.add(pickupTime);
+        orderDetails.add(isCurbside);
+        orderDetails.add(pickUpDate);
 
+        System.out.println(orderDetails);
         model.addAttribute("cart", cart);
 
         return "cart/cart-checkout-receipt";
@@ -96,8 +102,11 @@ public class CartController {
 
     //  CHECKOUT STOP 2
     @GetMapping("/Cart/checkout-receipt")
-    public String getCheckoutReceipt(Model model, @SessionAttribute("cart") List<Product> cart) {
+    public String getCheckoutReceipt(Model model, @SessionAttribute("cart") List<Product> cart, @SessionAttribute("orderDetails") List<String> orderDetails) {
+        model.addAttribute("orderDetails", orderDetails);
         model.addAttribute("cart", cart);
+
+        System.out.println(orderDetails);
 
         return "cart/cart";
     }
@@ -113,7 +122,7 @@ public class CartController {
 //            cart = (List<Product>) request.getSession().getAttribute("cart");
 //        }
 
-        return "cart/cart";
+        return "redirect:/Home";
     }
 
 
