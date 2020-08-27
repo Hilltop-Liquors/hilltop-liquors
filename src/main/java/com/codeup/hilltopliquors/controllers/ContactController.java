5b0446bfe8cad13a9b80b0254532ed4eea049bd6
Andrew Brought <andrewbrought@gmail.com>
2020-08-27 12:00:43 -0500

    user is able to contact store through email contact page

--- src/main/java/com/codeup/hilltopliquors/controllers/ContactController.java
+++ src/main/java/com/codeup/hilltopliquors/controllers/ContactController.java
@@ -1,11 +1,12 @@
 package com.codeup.hilltopliquors.controllers;
 
 import com.codeup.hilltopliquors.services.ContactEmailService;
-import com.codeup.hilltopliquors.services.EmailService;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ContactController {
@@ -24,12 +25,13 @@ public class ContactController {
 
 
     @PostMapping("/Contact")
+//    @ResponseBody
     public String postContact(@RequestParam String name, @RequestParam String phone,
                       @RequestParam String email, @RequestParam String message) {
 
 
-
-        contactEmailService.prepareAndSend(email, name, message + " Customer phone number: " + phone);
+        System.out.println("THIS IS THE EMAIL WE'RE LOOKING FOR: " + name + phone + email + message);
+        contactEmailService.prepareAndSend(email + "", name + "", message + " \nCustomer phone number: " + phone);
 
         return "redirect:/Home?emailSuccess";
     }
--- src/main/java/com/codeup/hilltopliquors/models/EmailExample.java
+++ src/main/java/com/codeup/hilltopliquors/models/EmailExample.java
@@ -9,13 +9,13 @@
 //
 //public class EmailExample {
 //    public static void main(String[] args) throws IOException {
-//        Email from = new Email("test@example.com");
+//        Email from = new Email("hilltopliquorstx@gmail.com");
 //        String subject = "Sending with SendGrid is Fun";
 //        Email to = new Email("andrewbrought@gmail.com");
 //        Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
 //        Mail mail = new Mail(from, subject, to, content);
 //
-//        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+//        SendGrid sg = new SendGrid(System.getenv(""));
 //        Request request = new Request();
 //        try {
 //            request.setMethod(Method.POST);
