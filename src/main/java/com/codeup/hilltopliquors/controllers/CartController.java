78937ba043111a650edfca7e032ca6404a19037c
Shelby Polasek <shelbypolasek@gmail.com>
2020-08-26 23:20:12 -0500

    order details rendering

--- src/main/java/com/codeup/hilltopliquors/controllers/CartController.java
+++ src/main/java/com/codeup/hilltopliquors/controllers/CartController.java
@@ -3,6 +3,7 @@ package com.codeup.hilltopliquors.controllers;
 import com.codeup.hilltopliquors.models.*;
 import com.codeup.hilltopliquors.repositories.*;
 import org.apache.poi.ss.usermodel.charts.ScatterChartData;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -124,7 +125,6 @@ public class CartController {
 //        }
 
 
-
 //        Order newOrder = new Order();
 ////        User user =(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 //        Instant instant = Instant.now();
@@ -141,8 +141,6 @@ public class CartController {
     }
 
 
-
-
     //  CHECKOUT STOP 1
     @GetMapping("/Cart/confirm-details")
     public String getCheckoutDetails(Model model, @SessionAttribute("cart") List<Product> cart, @SessionAttribute("orderDetails") List<String> orderDetails) {
@@ -152,12 +150,20 @@ public class CartController {
         return "cart/cart-checkout-details";
     }
 
-//    POST MAPPING FOR CONFIRM DETAILS
+    //    POST MAPPING FOR CONFIRM DETAILS
     @PostMapping("/Cart/confirm-details")
     public String saveCheckoutDetails(Model model, @SessionAttribute("cart") List<Product> cart, @SessionAttribute("orderDetails") List<String> orderDetails, String pickUpDate, String isCurbside, String pickupTime) {
-        orderDetails.add(pickupTime);
-        orderDetails.add(isCurbside);
         orderDetails.add(pickUpDate);
+        orderDetails.add(pickupTime);
+
+        if (isCurbside.equals("on")) {
+            orderDetails.add("curbside");
+        } else if (isCurbside == null) {
+            orderDetails.add("in-store");
+        }
+
+
+//    thymeleaf if cu
 
         System.out.println(orderDetails);
         model.addAttribute("cart", cart);
@@ -165,21 +171,55 @@ public class CartController {
         return "cart/cart-checkout-receipt";
     }
 
+
     //  CHECKOUT STOP 2
     @GetMapping("/Cart/checkout-receipt")
     public String getCheckoutReceipt(Model model, @SessionAttribute("cart") List<Product> cart, @SessionAttribute("orderDetails") List<String> orderDetails) {
         model.addAttribute("cart", cart);
 
-        model.addAttribute("orderDetails", orderDetails);
-        System.out.println(orderDetails);
+//        String str[] = num.split(" ");
 
-        return "cart/cart";
+//        String time = String.valueOf(orderDetails.subList(0,1));
+//        String wantCurbside = String.valueOf(orderDetails.subList(1,2));
+//        String pickupDate = String.valueOf(orderDetails.subList(2,3));
+//
+//String orderString;
+//        for(<l order : orderDetails){
+////            System.out.println(order.toString());
+//            System.out.println(order.split(","));
+//        }
+
+        //        String pickupDate = orderDetails.get(2);
+////
+//        System.out.println("TIME ===== " + time);
+//        System.out.println("WANT CURBSIDE ===== " + wantCurbside);
+//        System.out.println("PICKUP =======" + pickupDate);
+
+
+//        model.addAttribute("pickupTime", time);
+//        model.addAttribute("wantCurbside", wantCurbside);
+//        model.addAttribute("pickupDate", pickupDate);
+
+        List<String> titles = new ArrayList<>();
+        titles.add("Pickup Date");
+        titles.add("Pickup Time");
+        titles.add("Type Pickup");
+
+        System.out.println(titles);
+        model.addAttribute("titles", titles);
+        model.addAttribute("orderDetails", orderDetails);
+//        System.out.println(orderDetails);
+
+        return "cart/cart-checkout-receipt";
     }
-// POST MAPPING FOR SAVE ORDER AND SEND EMAIL
+
+    // POST MAPPING FOR SAVE ORDER AND SEND EMAIL
     @PostMapping("/Cart/checkout-receipt")
     public String saveCheckoutOrder
-            (@SessionAttribute("cart") List<Product> cart, @SessionAttribute("orderDetails") List<String> orderDetails) {
+    (@SessionAttribute("cart") List<Product> cart, @SessionAttribute("orderDetails") List<String> orderDetails) {
 //        List<Product> cart;
+
+
         cart.clear();
         orderDetails.clear();
 
