0210b78a2f8a2c8194f9b7d5da5b36b00129a545
Shelby Polasek <shelbypolasek@gmail.com>
2020-09-02 02:43:23 -0500

    side shop working

--- src/main/java/com/codeup/hilltopliquors/controllers/HomeController.java
+++ src/main/java/com/codeup/hilltopliquors/controllers/HomeController.java
@@ -1,18 +1,14 @@
 package com.codeup.hilltopliquors.controllers;
-import com.codeup.hilltopliquors.models.Product;
+import com.codeup.hilltopliquors.models.*;
+import com.codeup.hilltopliquors.repositories.*;
 import org.springframework.beans.factory.annotation.Value;
-import com.codeup.hilltopliquors.repositories.UserRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import com.codeup.hilltopliquors.models.User;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -22,14 +18,28 @@ import java.util.List;
 //@SessionAttributes("cart")
 public class HomeController {
 
+
     @Value("${cocktail.api.key}")
     private String cocktailApiKey;
 
     public final UserRepository userDao;
+    private final ProductRepository productDao;
+    private final SubcategoryRepository subCatDao;
+    private final CategoryRepository catDao;
+    private final ProductTypeRepository productTypeDao;
 
-    public HomeController(UserRepository userDao) {
+    public HomeController(UserRepository userDao, ProductRepository productDao, SubcategoryRepository subCatDao, CategoryRepository catDao, ProductTypeRepository productTypeDao) {
         this.userDao = userDao;
+        this.productDao = productDao;
+        this.subCatDao = subCatDao;
+        this.catDao = catDao;
+        this.productTypeDao = productTypeDao;
     }
+
+
+//    public HomeController(UserRepository userDao) {
+//        this.userDao = userDao;
+//    }
 
     @GetMapping("/")
     public String showLanding(HttpServletRequest request) {
@@ -50,8 +60,43 @@ public class HomeController {
     }
 
     @GetMapping("/Home")
-    public String showHome(Model model) {
+    public String showHome(Model model, String keyword, Integer catId, Integer subId, Integer productTypeId, Long ProductId) {
         model.addAttribute("cocktailApiKey", cocktailApiKey);
+
+            List<Product> products = productDao.findAll();
+            List<ProductType> productTypes = productTypeDao.findAll();
+            List<Category> categories = catDao.findAll();
+            List<Subcategory> subCategories = subCatDao.findAll();
+
+//      CATEOGRY TAGS
+            List<Product> catProducts = productDao.findAllBySubCategoryCategoryId(catId);
+//      SUB-CAT TAGS
+            List<Product> subProducts = productDao.findAllBySubCategoryId(subId);
+
+//        SEARCH BOX
+            if (keyword != null) {
+                model.addAttribute("products", productDao.findByKeyWord(keyword));
+                model.addAttribute("keyword", "Search results for: " + keyword);
+            } else if (catId != null) {
+                model.addAttribute("catProducts", catProducts);
+                model.addAttribute("subProducts", subProducts);
+            } else if (subId != null) {
+                System.out.println(subId);
+                model.addAttribute("catProducts", catProducts);
+                model.addAttribute("subProducts", subProducts);
+            } else if (productTypeId != null){
+                model.addAttribute("catProducts", catProducts);
+                model.addAttribute("subProducts", subProducts);
+            } else {
+                model.addAttribute("products", products);
+            }
+
+            model.addAttribute("productTypes", productTypes);
+            model.addAttribute("categories", categories);
+
+//            return "home/home";
+
+
         return "home";
     }
 
@@ -81,65 +126,61 @@ public class HomeController {
         return "user/index";
     }
 
-//////    add confirm field then run if else
-//    if( confirm username field != existing database.name ){
-//        save confirm username field
-//    } else if (confirm username field == database name ){
-//        throw error.
-//    } else if (confirm username field.equals(" ") ){
-//        save first username field;
-//    }
-    //    @ModelAttribute("ValidUser")
-//    public String isValidUser(HttpServletRequest request) {
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        if (request.getSession().getAttribute("auth") == null) {
-//            request.getSession().setAttribute("auth", auth);
-//        }
-//        return "auth";
-
-//        String username = request.getParameter("username");
-////        String password = request.getParameter("password");
-//        User userName = userDao.findByUsername(username);
-//        User userEmail = userDao.findByEmail();
+    //        GET ALL POSTS and SEARCH BAR
+//    @GetMapping("/HomeShop")
+//    public String getProducts(Model model, String keyword, Integer catId, Integer subId, Integer productTypeId, Long ProductId) {
+//        List<Product> products = productDao.findAll();
+//        List<ProductType> productTypes = productTypeDao.findAll();
+//        List<Category> categories = catDao.findAll();
+//        List<Subcategory> subCategories = subCatDao.findAll();
 //
-////        if (user == null) {
-////            response.sendRedirect("/login");
-////            return;
-////        }
+////      CATEOGRY TAGS
+//        List<Product> catProducts = productDao.findAllBySubCategoryCategoryId(catId);
+////      SUB-CAT TAGS
+//        List<Product> subProducts = productDao.findAllBySubCategoryId(subId);
 //
-//        boolean validAttempt = userName ;
-//
-//        if (validAttempt) {
-//            request.getSession().setAttribute("user", user);
-//            response.sendRedirect("/profile");
+////        SEARCH BOX
+//        if (keyword != null) {
+//            model.addAttribute("products", productDao.findByKeyWord(keyword));
+//            model.addAttribute("keyword", "Search results for: " + keyword);
+//        } else if (catId != null) {
+//            model.addAttribute("catProducts", catProducts);
+//            model.addAttribute("subProducts", subProducts);
+//        } else if (subId != null) {
+//            System.out.println(subId);
+//            model.addAttribute("catProducts", catProducts);
+//            model.addAttribute("subProducts", subProducts);
+//        } else if (productTypeId != null){
+//            model.addAttribute("catProducts", catProducts);
+//            model.addAttribute("subProducts", subProducts);
 //        } else {
-//            response.sendRedirect("/login");
+//            model.addAttribute("products", products);
 //        }
-//    }
-    //    @GetMapping("/Contact")
-//    public String showContact() {
-//        return "contact";
-//    }
-
-//    @GetMapping("/Recipes")
-//    public String showRecipes() {
-//        return "recipes";
+//
+//        model.addAttribute("productTypes", productTypes);
+//        model.addAttribute("categories", categories);
+//
+//        return "home/home";
 //    }
 
-//    @GetMapping("/Register")
-//    public String showSignUp() {
-//        return "registration";
-//    }
+    @PostMapping("/Home")
+    public String addToCart(HttpServletRequest request, Model model, @SessionAttribute("cart") List<Product> cart, Long productId) {
+        List<Product> products = productDao.findAll();
+        model.addAttribute("products", products);
 
-//    @GetMapping("/Login")
-//    public String showLogin() {
-//        return "login";
-//    }
+        if (request.getSession().getAttribute("cart") == null) {
+            cart = new ArrayList<>();
+        } else {
+            cart = (List<Product>) request.getSession().getAttribute("cart");
+        }
 
-//    @GetMapping("/Search")
-//    public String showSearch() {
-//        return "search";
-//    }
+
+        Product product = productDao.getOne(productId);
+        cart.add(product);
+        request.getSession().setAttribute("cart", cart);
+//        return "search/search";
+        return  "redirect:/Home?success";
+    }
 
 
 
